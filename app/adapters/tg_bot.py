b2dcd5d3034a3f7227b497cf6e6775e7ce93d889
Administrator <vladislav@ridenow.tech>
2026-01-28 17:20:57 +0000

    -

---
+++ app/adapters/tg_bot.py
@@ -0,0 +1,12 @@
+import telebot
+from app.config import CAR_BOT_TOKEN
+
+
+class TG_BOT:
+    def __init__(self, token):
+        self.bot = telebot.TeleBot(token)
+
+    @staticmethod
+    async def send_message_to_tg(message_text, chat_id):
+        bot = telebot.TeleBot(CAR_BOT_TOKEN)
+        bot.send_message(chat_id=chat_id, text=message_text, parse_mode='html', disable_web_page_preview=True)
